3d1a0b568eaa717a63164f55b2a39e3acf0e8e7b
mahisurrahman <zordanblog009@gmail.com>
2025-06-18 03:16:18 +0600

    Day View ready

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.css";
+import CalendarHeader from "./components/CalendarHeader/CalendarHeader";
+import Month from "./components/Month/Month";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { getMonth } from "./utils/utils";
+
+function App() {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+  return (
+    <React.Fragment>
+      <div className="h-screen flex flex-col ">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
